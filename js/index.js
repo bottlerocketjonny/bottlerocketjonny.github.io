6c9907e279db7eac1989d128103b8af4d7278256
Jonny Coddington <bottlerocketjonny@protonmail.com>
2022-10-07 19:52:11 +0100

    v3
    Changed some of the layout. Updated text.

--- js/index.js
+++ js/index.js
@@ -11,3 +11,10 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
